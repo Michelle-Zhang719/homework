30b7d3256fa94c17f9a8d56c709441fe227a68f6
Michelle-Zhang719 <zhangminjie719@163.com>
2023-03-03 21:55:08 +0800

    开第二部分 面向对象 day5：上午---卷4 数组后两节 +卷5 二维数组和递归 下午---卷1 面向对象基础  第1节---第4节

---
+++ src/month1/week1/day5/Question4.java
@@ -0,0 +1,52 @@
+package month1.week1.day5;
+
+import java.util.Arrays;
+
+/**
+ * 数组综合练习——真正删除元素
+ * 对于某个String类型数组，将其中的某个元素全部去掉，得到一个新数组，并统计去掉了几个元素。
+ * 举例，对于String数组["abc", "123", "123", "123", "666", "777"] 将其中的元素"123"全部去掉，就得到了新数组["abc","666", "777"]，一共去掉了3个元素。
+ * 注：
+ * 这里说的去掉，不是指用0/null等默认值替代原先的元素，而是指真正的删除掉。
+ * 思考：同一个数组能不能实现这个功能？如果不能，应该怎么完成呢？
+ * String如何判断内容相同？
+ * <p>
+ * 答：
+ * 不能，数组一旦被定义，其长度不可更改
+ * 使用equals方法
+ *
+ * @Author MinjieZhang
+ * @Date 2023-03-03 20:32
+ */
+public class Question4 {
+    public static void main(String[] args) {
+        String[] str = {"abc", "123", "123", "123", "666", "777"};
+        //被去掉的数组
+        String ele = "123";
+        /*
+        思路：
+        遍历string数组，每找到目标元素，就new一个新数组，直至遍历结束
+         */
+        delete(str, ele);
+
+    }
+
+    public static void delete(String[] str, String target) {
+
+        //统计去掉的相同的元素的个数
+        int count = 0;
+        //为新数组元素下标
+        String[] newStr = new String[str.length - 1];
+        int newIndex = 0;
+        for (int i = 0; i < str.length; i++) {
+            if (target.equals(str[i]) == false) {
+                //在数组中没有找到目标元素
+                newStr[newIndex] = str[i];
+                count++;
+                newIndex++;
+            }
+        }
+        System.out.println("删除之前数组为 " + Arrays.toString(str));
+        System.out.println("删除”123“元素后，数组为 " + Arrays.toString(newStr));
+    }
+}
---
+++ src/month1/week1/day5/Question4_correct.java
@@ -0,0 +1,60 @@
+package month1.week1.day5;
+
+import java.util.Arrays;
+
+/**
+ * 对于某个String类型数组，将其中的某个元素全部去掉，得到一个新数组，并统计去掉了几个元素。
+ * 举例，对于String数组["abc", "123", "123", "123", "666", "777"] 将其中的元素"123"全部去掉，就得到了新数组["abc","666", "777"]，一共去掉了3个元素。
+ *
+ * @Author MinjieZhang
+ * @Date 2023-03-03 21:40
+ */
+public class Question4_correct {
+    public static void main(String[] args) {
+        String[] oldArr = {"123", "456", "789", "123", "123", "666", "777"};
+        // 获取删除元素后新数组的长度
+        String target = "123";
+        int delNum = getDeleteElementNum(oldArr, target);
+        // 创建新数组 直接减掉重复元素的个数
+        String[] newArr = new String[oldArr.length - delNum];
+        deleteElement(oldArr, newArr, target);
+
+        // 遍历查看新数组
+        System.out.println(Arrays.toString(newArr));
+    }
+
+    //传入两个数组，删除old数组中的target元素，得到一个新数组
+    public static void deleteElement(String[] oldArr, String[] newArr, String target) {
+        // 新数组的下标
+        int newIndex = 0;
+        //遍历old数组
+        for (int i = 0; i < oldArr.length; i++) {
+            // null元素跳过,避免空指针异常
+            if (oldArr[i] == null) {
+                continue;
+            }
+            // 找到不是目标元素的元素复制进新数组
+            if (!oldArr[i].equals(target)) {
+                newArr[newIndex] = oldArr[i];
+                //赋值后数组下标+1
+                newIndex++;
+            }
+        }
+    }
+
+    //获取一个数组中目标数据的个数
+    public static int getDeleteElementNum(String[] oldArr, String target) {
+        // 计数器
+        int count = 0;
+        for (int i = 0; i < oldArr.length; i++) {
+            // null元素跳过,避免空指针异常
+            if (oldArr[i] == null) {
+                continue;
+            }
+            if (oldArr[i].equals(target)) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
